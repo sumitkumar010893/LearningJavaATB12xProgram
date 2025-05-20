b20b9d17c831025f04135be334d46dc315454fdd
Sumit Kumar <ceb.sumitkumar@gmail.com>
2025-05-20 20:43:30 +0530

    first commit

--- src/main/java/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/main/java/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -2,6 +2,6 @@ package ex_01_Java_Basics;
 
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi Sumit, How are you?");
+        System.out.println("Hello World");
     }
 }
